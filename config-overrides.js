b4af5f63d78671bf9fbcf9138a5f0abe08b1290e
zane-programs <zstjohn22@windwardschool.org>
2022-04-25 18:41:23 -0700

    2.3.5 - override react webpack config to split js into chunks, clean up config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,32 +1,31 @@
 const { execSync } = require("child_process");
-// const { readdirSync, readFileSync } = require("fs");
-// const { join: joinPath } = require("path");
-
 const { DefinePlugin } = require("webpack");
 
 // get data from package.json
 const packageJson = require("./package.json");
 
-// // get file names for naviance data
-// const navianceDataFileNames = readdirSync(joinPath(__dirname, "./data"));
-
-// // put naviance data in array
-// let navianceData = [];
-// for (const file of navianceDataFileNames) {
-//   navianceData.push(
-//     // parse json
-//     JSON.parse(readFileSync(joinPath(__dirname, "./data/" + file)).toString())
-//   );
-// }
-
+// get commit hash from git
 const commitHash = execSync("git rev-parse HEAD").toString().trim();
 
-module.exports = function override(config) {
-  // add my extras
+module.exports = function override(config, webpackEnv) {
+  // add optimization and plugins if not already there
+  if (!config.optimization) config.optimization = {};
   if (!config.plugins) config.plugins = [];
+
+  // in dev, don't split into chunks. in prod, do!
+  // thanks to (not yet merged) pr:
+  // https://github.com/facebook/create-react-app/pull/12213
+  config.optimization.splitChunks =
+    webpackEnv === "development"
+      ? undefined
+      : {
+          chunks: "all",
+        };
+
+  // inject data into bundle (currently just
+  // app version and commit hash)
   config.plugins.push(
     new DefinePlugin({
-      // __NAVIANCE_DATA__: JSON.stringify(navianceData),
       __APP_VERSION__: JSON.stringify(packageJson.version),
       __COMMIT_HASH__: JSON.stringify(commitHash),
     })
